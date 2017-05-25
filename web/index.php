002847b9a3eea1349ca26f7d426dd7b61bd99f3a
Joris Rietveld <jorisrietveld@gmail.com>
2017-05-25 18:12:19 +0200

    Added config files and index page.

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Author: Joris Rietveld <jorisrietveld@gmail.com>
+ * Created: 25-05-2017 15:27
+ * Licence: GNU General Public licence version 3 <https://www.gnu.org/licenses/quick-guide-gplv3.html>
+ */
+declare(strict_types=1);
+
